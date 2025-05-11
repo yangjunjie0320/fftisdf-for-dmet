64957bfafcbad64a73aec399e4906909729d8e71
Junjie Yang <yangjunjie0320@gmail.com>
2025-05-11 16:48:07 -0700

    update

--- test/test-klno/run.sh
+++ test/test-klno/run.sh
@@ -1,14 +1,17 @@
 source /Users/yangjunjie/anaconda3/bin/activate fftisdf
-which python
-export PYTHONPATH=$PWD/../../libdmet2-main/:$PYTHONPATH
-export PYTHONPATH=$PWD/../../fftisdf-main/:$PYTHONPATH
-export PYTHONPATH=$PWD/../../lno-klno/:$PYTHONPATH
+
+export PYTHONPATH=$PWD/../../src/libdmet2-main/:$PYTHONPATH
+export PYTHONPATH=$PWD/../../src/fftisdf-main/:$PYTHONPATH
+export PYTHONPATH=$PWD/../../src/lno-klno/:$PYTHONPATH
+export PYTHONPATH=$PWD/../../src/scripts/:$PYTHONPATH
+
 export PYSCF_TMPDIR=$PWD/tmp/
 
 python -c "import pyscf; print(pyscf.__file__)"
 python -c "import libdmet; print(libdmet.__file__)"
 python -c "import fft; print(fft.__file__)"
+python -c "import lno.base; print(lno.base.__file__)"
 
-python test_patch.py
+python test.py
 
 rm -rf *h5 *json 2>/dev/null
