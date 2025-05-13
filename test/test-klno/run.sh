bba5546fe257563a0ac76c5b552cf3c4fe5e4b5c
Junjie Yang <yangjunjie0320@gmail.com>
2025-05-13 16:02:37 -0700

    update

--- test/test-klno/run.sh
+++ test/test-klno/run.sh
@@ -1,17 +1,44 @@
-source /Users/yangjunjie/anaconda3/bin/activate fftisdf
+#!/bin/bash
+#SBATCH --job-name=nio-afm
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem-per-cpu=6gb
+#SBATCH --time=00:30:00
+#SBATCH --constraint=icelake
+
+echo "SLURMD_NODENAME = $SLURMD_NODENAME"
+echo "Start time = $(date)"
+source /home/junjiey/anaconda3/bin/activate fftisdf-with-mkl
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK;
+export MKL_NUM_THREADS=$SLURM_CPUS_PER_TASK;
+export OPENBLAS_NUM_THREADS=$SLURM_CPUS_PER_TASK;
+export PYSCF_MAX_MEMORY=$((SLURM_MEM_PER_CPU * SLURM_CPUS_PER_TASK))
+
+echo OMP_NUM_THREADS = $OMP_NUM_THREADS
+echo MKL_NUM_THREADS = $MKL_NUM_THREADS
+echo OPENBLAS_NUM_THREADS = $OPENBLAS_NUM_THREADS
+echo PYSCF_MAX_MEMORY = $PYSCF_MAX_MEMORY
 
 export PYTHONPATH=$PWD/../../src/libdmet2-main/:$PYTHONPATH
 export PYTHONPATH=$PWD/../../src/fftisdf-main/:$PYTHONPATH
 export PYTHONPATH=$PWD/../../src/lno-klno/:$PYTHONPATH
 export PYTHONPATH=$PWD/../../src/scripts/:$PYTHONPATH
 
-export PYSCF_TMPDIR=$PWD/tmp/
+export TMP=/resnick/scratch/yangjunjie/
+export TMPDIR=$TMP/$SLURM_JOB_NAME/$SLURM_JOB_ID/
+export PYSCF_TMPDIR=$TMPDIR
 
-python -c "import pyscf; print(pyscf.__file__)"
-python -c "import libdmet; print(libdmet.__file__)"
-python -c "import fft; print(fft.__file__)"
-python -c "import lno.base; print(lno.base.__file__)"
+mkdir -p $TMPDIR
+echo TMPDIR       = $TMPDIR
+echo PYSCF_TMPDIR = $PYSCF_TMPDIR
+ln -s $PYSCF_TMPDIR tmp
+
+export PYTHONPATH=$PWD/../../src/libdmet2-main/:$PYTHONPATH
+export PYTHONPATH=$PWD/../../src/fftisdf-main/:$PYTHONPATH
+export PYTHONPATH=$PWD/../../src/lno-klno/:$PYTHONPATH
+export PYTHONPATH=$PWD/../../src/scripts/:$PYTHONPATH
 
 python test.py
 
-rm -rf *h5 *json 2>/dev/null
+echo "End time = $(date)"
