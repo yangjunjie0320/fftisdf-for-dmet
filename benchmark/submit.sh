49b6fa71c60e116396f2b9c066132f3e192ad0a4
Junjie Yang <yangjunjie0320@gmail.com>
2025-07-09 14:48:23 -0700

    update

--- benchmark/submit.sh
+++ benchmark/submit.sh
@@ -5,8 +5,7 @@ for cell in diamond; do
     mkdir -p $cell
 
     cd $cell; cp ../../submit.py .
-    python submit.py --cell $cell --method $method \
-                     --time 12:00:00 --cpus-per-task 32
+    python submit.py --time 12:00:00 --cpus-per-task 32
 
     cd ..
 done
