133f49b296ecb29c850fa3c2bea8d5896a05ffe0
Junjie Yang <yangjunjie0320@gmail.com>
2025-07-09 23:37:55 -0700

    update

--- benchmark/kmesh/diamond/3-3-3/fftdf-60/run.sh
+++ benchmark/kmesh/diamond/3-3-3/fftdf-60/run.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=32
 #SBATCH --mem-per-cpu=8gb
-#SBATCH --time=12:00:00
+#SBATCH --time=20:00:00
 
 echo "SLURMD_NODENAME = $SLURMD_NODENAME"
 echo "Start time = $(date)"
