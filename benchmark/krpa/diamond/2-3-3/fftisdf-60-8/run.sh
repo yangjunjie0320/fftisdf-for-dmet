6486d9d66f6b1726705b9b89e487e63dc24a5843
Junjie Yang <yangjunjie0320@gmail.com>
2025-07-14 11:21:17 -0700

    update

--- benchmark/krpa/diamond/2-3-3/fftisdf-60-8/run.sh
+++ benchmark/krpa/diamond/2-3-3/fftisdf-60-8/run.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 #SBATCH --reservation=changroup_standingres
-#SBATCH --qos=debug
 #SBATCH --job-name=diamond-fftisdf-60-8
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=32
 #SBATCH --mem-per-cpu=8gb
-#SBATCH --time=00:10:00
+#SBATCH --time=20:00:00
 
 echo "SLURMD_NODENAME = $SLURMD_NODENAME"
 echo "Start time = $(date)"
--- benchmark/submit.sh
+++ benchmark/submit.sh
@@ -5,7 +5,7 @@ for cell in diamond; do
     mkdir -p $cell
 
     cd $cell; cp ../../submit.py .
-    python submit.py --time 00:10:00 --cpus-per-task 32
+    python submit.py --time 20:00:00 --cpus-per-task 32
 
     cd ..
 done
