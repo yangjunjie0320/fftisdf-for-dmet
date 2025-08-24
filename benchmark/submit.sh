bdef58c8be8ebe14e7c7112010d922055f1f4a2d
Junjie Yang <yangjunjie0320@gmail.com>
2025-08-24 16:42:22 -0700

    update

--- benchmark/submit.sh
+++ benchmark/submit.sh
@@ -1,11 +1,12 @@
-method="krpa"
+method="ref-kpts"
 mkdir -p $method; cd $method
 
 for cell in diamond; do
     mkdir -p $cell
 
     cd $cell; cp ../../submit.py .
-    python submit.py --time 20:00:00 --cpus-per-task 32
+    python submit.py --time="20:00:00" --cpus-per-task="32" \
+    --reservation="changroup-h100-node-1"
 
     cd ..
 done
